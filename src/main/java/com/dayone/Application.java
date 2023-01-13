e37b8ea94a8418fa955e339cb4ba03cc385761f2
paik-dongjoo <d5ngjoo@naver.com>
2023-01-14 00:11:42 +0900

    실전 배당금 프로젝트 Chapter 02. 스크래핑
    - gradle-wrapper.properties : 오타 수정
    - 웹사이트 분석 및 스크래핑 구현
    - 예시 : 야후 파이낸스 - 코카콜라 배당금 정보
    * 해당 사이트에 맞춰진 예시 코드이며, 단순 HTML 구조가 아닌 원하는 데이터를 파악하고 사용할 수 있도록 분석하는 경험 필요

--- src/main/java/com/dayone/Application.java
+++ src/main/java/com/dayone/Application.java
@@ -1,13 +1,49 @@
 package com.dayone;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+//		SpringApplication.run(Application.class, args);
+
+        // 스크래핑 이해를 위한 메인 메소드 상 코드 구현 선행
+        // 해당 페이지의 HTML 구조에만 집중할 것이 아닌 원하는 데이터를 파악하고 분석하는 경험 필요
+        try {
+            Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/COKE/history?period1=99100800&period2=1673568000&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true");
+            Document document = connection.get();
+
+            Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
+            Element ele = eles.get(0);    // table 전체 데이터
+
+            Element tbody = ele.children().get(1);    // table -> tbody 데이터
+            for (Element e : tbody.children()) {
+                String txt = e.text();
+                if (!txt.endsWith("Dividend")) {
+                    continue;
+                }
+
+                String[] splits = txt.split(" ");
+                String month = splits[0];
+                int day = Integer.valueOf(splits[1].replace(",", ""));
+                int year = Integer.valueOf(splits[2]);
+                String dividend = splits[3];
+
+                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
