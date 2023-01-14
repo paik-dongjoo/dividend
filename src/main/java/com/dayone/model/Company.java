f1c4839c1d94e9379926fe54ca277351164e38ee
paik-dongjoo <d5ngjoo@naver.com>
2023-01-15 04:41:31 +0900

    실전 배당금 프로젝트 Chapter 04. 서비스 구현(1~5)
    - [배당금 저장]
    - 1) ticker 기준 회사 정보 스크래핑
    - 2) 회사 존재 시 회사 배당금 정보 스크래핑
    - 3) 스크래핑 결과 Company 리턴

---
+++ src/main/java/com/dayone/model/Company.java
@@ -0,0 +1,17 @@
+package com.dayone.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Company {
+
+    // Entity 가 있음에도 model class 별도 정의 이유
+    // Entity class 는 database table 과 직접 매핑 목적
+    // 데이터를 주고받거나 변경하는 등 로직이 적용되면 본래 역할을 벗어나게 될 수 있음
+    // 하나의 클래스에 여러 기능을 적용하면 사이드 이펙트가 발생할 가능성 또한 증가함
+
+    private String ticker;
+    private String name;
+}
